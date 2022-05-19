6ec6f22ee0543636a30f792d4165414af6afe3a3
Mahmud <60064090+mahmudyusifli@users.noreply.github.com>
2022-05-19 19:08:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// const bookings = ['salam'];
+// const createbooking = function (flightnum, numpassangers, price) {
+//   numpassangers = numpassangers || 1;
+//   price = price || 199;
+//   const booking = {
+//     flightnum,
+//     numpassangers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createbooking('lh123');
+// console.log('---');
+// console.log(bookings);
+
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+// const greeterhey = greet('hey,');
+// greeterhey('steven');
+// greeterhey('sdffd');
+
+// const greeterhey = function (name) {
+//   console.log(`hey, ${name}`);
+// };
+//using arrow function
+// const greet = greeting => name => console.log(`${greeting} ${name}`);
+
+// greet('hello')('druzya');
+console.log(sum(5, 9));
+const sum = (a, b) => a + b;
